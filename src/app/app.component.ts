748fc46581c07ea383a3dd2d116ddff138c9d04d
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2019-02-25 23:12:05 +0200

    Generated the initial Angular 5 application with the CLI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular5-complete-guide';
+  title = 'Angular 5 - Complete Guide';
+  name = 'starting point';
 }
