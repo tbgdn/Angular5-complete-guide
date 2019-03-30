7ff2680700491edd68016eaa4ed19d783b6ed468
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2019-03-31 01:15:26 +0200

    Started the code for Chapter 02

---
+++ ch02/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  exports: [
+    ServerComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
