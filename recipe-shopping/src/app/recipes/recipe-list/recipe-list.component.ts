bf18c44d8061eeedbb7c33ea6f94adeb3c50a49a
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2019-04-02 00:19:17 +0300

    Added navigation, master-detail interaction and shopping list functionality.

--- recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,9 +7,12 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Pizza', 'This is just a test', 'https://cdn.pixabay.com/photo/2017/12/05/20/09/pizza-3000274_960_720.jpg'),
-    new Recipe('Pizza', 'This is just a test', 'https://cdn.pixabay.com/photo/2017/12/05/20/09/pizza-3000274_960_720.jpg'),
+    new Recipe('Pizza', 'This is how we make the dough...', 'https://cdn.pixabay.com/photo/2017/12/05/20/09/pizza-3000274_960_720.jpg'),
+    new Recipe('Pasta', 'First, you have to boil the pastas',
+      'https://www.maxpixel.net/static/photo/1x/Pasta-Hog-Italian-Recipe-Food-Truffle-Lunch-3663720.jpg'),
+    new Recipe('Burger', 'Grab a good piece of meat', 'https://cdn.pixabay.com/photo/2015/04/20/13/25/burger-731298_960_720.jpg'),
   ];
 
   constructor() { }
@@ -17,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
--- recipe-shopping/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-shopping/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  addIngredient() {
+    this.ingredientAdded.emit(new Ingredient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value));
+  }
 }
