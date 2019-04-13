acace035b373efc435e0e3726b02d74e743ae339
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2019-04-13 23:53:05 +0300

    Started the project for Chapter 13.

---
+++ ch13/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import {UserService} from './users.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
